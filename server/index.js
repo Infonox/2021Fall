5411943bd8f9958fe2123fa84ca0e75191d202ec
Henry Maldonado <infonox67@gmail.com>
2021-10-14 16:37:53 -0400

    Heroku Deployment

--- server/index.js
+++ server/index.js
@@ -1,33 +1,11 @@
-
-/* const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-*/
-
-const express = require('express')
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-.get('/newpaltz',(req,res) =>{
+app.use(serveStatic(path.join(__dirname, '../docs')));
 
-  res.send('Hello New Paltz!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
