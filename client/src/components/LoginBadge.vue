d6e36d01475d4b02807b0b711e7ffb1e9e12b522
Henry Maldonado <infonox67@gmail.com>
2021-10-14 16:06:58 -0400

    Login Functionality/Flow

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -1,38 +1,37 @@
 <template>
-  <div class="buttons" v-if="!Session.user">
-    <a class="button is-primary">
-      <strong>Sign up</strong>
-    </a>
-    <a class="button is-light" @click.prevent="login" > Log in </a>
-    
-  </div>
-
-  <div v-else> Hello {{name}} </div>
-  
- 
-</template> 
+    <div class="buttons" v-if="!Session.user">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light" @click="login">
+            Log in
+          </a>
+    </div>
+    <div v-else>
+        Hello {{name}} 
+    </div>
+</template>
 
 <script>
 import Session from "../services/session";
-
 export default {
-  data() {
-    return {
-      Session,
-    };
-  },
-  methods: {
-    login() {
-      this.$router.push("/login");
-      //this.Session.Login();
+    data (){
+        return ({ 
+            Session
+        })
     },
-  },
-  computed: {
-    name() {
-      return this.Session.user.firstName + " " + this.Session.user.lastName;
+    methods: {
+        login(){
+            this.$router.push('/login');
+            //this.Session.Login();
+        }
     },
-  },
-};
+    computed:{
+        name(){
+            return this.Session.user.firstName + ' ' + this.Session.user.lastName;
+        }
+    }
+}
 </script>
 
 <style>
