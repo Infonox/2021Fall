87ca630b330c702f24ca0c7805c9cce5354d93cc
Henry Maldonado <infonox67@gmail.com>
2021-10-25 14:39:07 -0400

    dotenv

--- server/index2.js
+++ server/index2.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path=require('path');
+require('dotenv').config();
+
+console.log(`The best class in New Paltz is ${process.env.BEST_CLASS}`)
+
 const usersController = require('./controllers/users');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app
 .use('/', express.static(path.join(__dirname, '../docs')))
 .use('/users', usersController)
